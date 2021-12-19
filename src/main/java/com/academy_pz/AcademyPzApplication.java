b2862238bd989db1676fcffe02ded34da8ef9405
WenziLike <SlavaMyraxin@icloud.com>
2021-12-19 17:08:45 +0100

    add Start Application file

---
+++ src/main/java/com/academy_pz/AcademyPzApplication.java
@@ -0,0 +1,13 @@
+package com.academy_pz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyPzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyPzApplication.class, args);
+    }
+
+}
